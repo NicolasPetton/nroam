6b5fd99e2e3cc40e564e76ed568807d87221e745
Damien Pollet <damien.pollet@gmail.com>
2021-03-14 17:48:20 +0100

    Organize backlinks under single heading

--- nroam-utils.el
+++ nroam-utils.el
@@ -34,6 +34,16 @@
   (let ((inhibit-read-only t))
     (unless (eq ?\n (char-before (1- (point)))) (insert "\n"))))
 
+(defun nroam--insert-heading (level title &optional tags)
+  "Insert a section heading"
+  (let ((pos (point))
+        (stars (make-string level ?*)))
+    (insert stars " " title "\n")
+    (when tags
+      (save-excursion
+      (goto-char pos)
+      (org-set-tags tags)))))
+
 (defun nroam--do-separated-by-newlines (function sequence)
   "Apply FUNCTION to each element of SEQUENCE.
 Insert a single newline between each call to FUNCTION."
